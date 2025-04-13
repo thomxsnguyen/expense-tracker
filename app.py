0b17f2bd4ba485d21fae9986168aa0b152b35ce3
Thomas <thomasn8255@gmail.com>
2025-04-12 19:15:11 -0700

    finished register route

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
 from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 import jwt
 
 # token = jwt.encode({'user': username}, app.config['SECRET_KEY'], algorithm="HS256")
 
 USER = {'user': "", 'password': ''}
 
-
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'super_secret_key' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Expense(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  category = db.Column(db.String, nullable=False)
+  expense = db.Column(db.Float, nullable=False)
 
 @app.route('/')
 def home():
-  render_template("index.html")
+  return render_template("index.html")
 
 @app.route('/register', methods=['POST'])
 def register():
-  data = request.get_json()
-  if not USER
-  USER['user'] = data.get('user')
-  USER['password'] = data.get('password')
+  try:
+    data = request.get_json()
+    username = data.get('user')
+    password = data.get('password')
 
-  return jsonify({'message': "registered"}), 200
+    if USER['user'] and USER['password']:
+      return jsonify({'message': 'user has already been registered'})
 
-
+    if not username or not password:
+      return jsonify({'message': 'username or password field is empty'}), 402
+    
+    USER['user'] = data.get('user')
+    USER['password'] = data.get('password')
+    return jsonify({'message': 'created'}), 200
+  except Exception as e:
+    return jsonify({'message': e}), 402
+  
 
 
 
