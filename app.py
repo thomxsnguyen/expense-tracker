9f11620b62349005b07c7dd57f3dd0b31b228989
Thomas <thomasn8255@gmail.com>
2025-04-13 19:53:10 -0700

    created user model in relation of expenses

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+
 import jwt
 
 # token = jwt.encode({'user': username}, app.config['SECRET_KEY'], algorithm="HS256")
@@ -14,10 +16,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), nullable=False)
+  password_hash = db.Column(db.String(128), nullable=False)
+  expenses = db.relationship('Expenses', backref="user", lazy=True)
+  
 class Expense(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  category = db.Column(db.String, nullable=False)
-  expense = db.Column(db.Float, nullable=False)
+  category = db.Column(db.String(50), nullable=False)
+  description = db.Column(db.String(100), nullable=False)
+  price = db.Column(db.Float, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+
 
 @app.route('/')
 def home():
@@ -30,29 +44,25 @@ def register():
     username = data.get('user')
     password = data.get('password')
 
-    if USER['user'] and USER['password']:
-      return jsonify({'message': 'user has already been registered'})
-
-    if not username or not password:
-      return jsonify({'message': 'username or password field is empty'}), 402
     
-    USER['user'] = data.get('user')
-    USER['password'] = data.get('password')
     return jsonify({'message': 'created'}), 200
   except Exception as e:
-    return jsonify({'message': e}), 402
+    return jsonify({'error': e}), 402
   
 
 @app.route('/login', methods=['POST'])
 def login():
-  data = request.get_json()
-  username = data.get('username')
-  password = data.get('password')
+  try:
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
 
-  if username == USER['username'] and password == USER['password']:
-    token = jwt.encode({'user': username}, app.config['SECRET_KEY'], algorithim="HS256")
-    return jsonify({'token': token}), 200
-  return jsonify({'message': 'invalid credentials'}), 402
+    if username == USER['username'] and password == USER['password']:
+      token = jwt.encode({'user': username}, app.config['SECRET_KEY'], algorithim="HS256")
+      return jsonify({'token': token}), 200
+    return jsonify({'message': 'invalid credentials'}), 402
+  except Exception as e:
+    return jsonify({'error': e})
 
 
   
@@ -61,4 +71,6 @@ def login():
 
 
 if __name__ == "__main__":
+  with app.app_context():
+    db.create_all()
   app.run(debug=True)
